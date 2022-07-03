b8cea2f7b1588344f8b48641dfa4e6b7ae66e8e0
sriraam <sriraam@QB-TP3-2037>
2022-07-03 17:17:32 +0530

    pavi commit

--- git/src/git/git.java
+++ git/src/git/git.java
@@ -3,12 +3,10 @@ package git;
 public class git {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("sample");
+		
+System.out.println("pavi");
 
-System.out.println("2sample");
 
-System.out.println("branch");
 	}
 
 }
